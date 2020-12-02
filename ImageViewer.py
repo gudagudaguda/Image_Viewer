974d5b2447bd00f623964e971b025bf81ea8796c
g09050zz <zhongtian.zheng@student.manchester.ac.uk>
2020-12-02 07:48:05 +0000

    ImageViewer_v1

---
+++ ImageViewer.py
@@ -0,0 +1,117 @@
+from tkinter import *
+from PIL import ImageTk, Image
+from tkinter import filedialog
+
+root = Tk()
+root.title('Image Viewer')
+	
+def openfile():
+	root.filename = filedialog.askopenfilenames(initialdir="/home", #Default location
+											title="Select file", 
+											filetypes=(("webp file",".webp"),("jpg file",".jpg"),("png file",".png"),("all file",".*")))
+	if not root.filename:
+		quit()
+	global ImageList
+	ImageList=[]
+
+def resize(img):
+	width = img.size[0]
+	height = img.size[1]
+	if img.size[1]>700:		
+		img = img.resize((int(700*width/height),700), Image.ANTIALIAS)
+
+	width = img.size[0]
+	height = img.size[1]
+	if  img.size[0]>1920:
+		img = img.resize((1600,int(1600*height/width)), Image.ANTIALIAS)
+	ImageList.append(ImageTk.PhotoImage(img))
+
+def load(num):
+	img = Image.open(root.filename[num])
+	resize(img)
+
+def iniprintImg(num):
+	global myLabel
+	myLabel=Label(image=ImageList[num])
+	myLabel.grid(row=0, column=0, columnspan=3)
+	status = Label(root,text="Image "+str(num+1)+" of "+str(len(root.filename))+"	", bd=1, relief=SUNKEN, anchor=E, pady=5)
+	status.grid(row=2,column=0,columnspan=3, sticky=W+E)
+
+def printImg(num):
+	global myLabel
+	myLabel.grid_forget()
+	myLabel=Label(image=ImageList[num])
+	myLabel.grid(row=0, column=0, columnspan=3)
+	status = Label(root,text="Image "+str(num+1)+" of "+str(len(root.filename))+"	", bd=1, relief=SUNKEN, anchor=E, pady=5)
+	status.grid(row=2,column=0,columnspan=3, sticky=W+E)
+
+def inibutton():
+	button_back = Button(root, text="<<", state=DISABLED, pady=5)
+	button_exit = Button(root, text="OPEN", bg='#d97b7b', command=openfiles, pady=5)
+	if len(root.filename)==1:
+		button_forward = Button(root, text=">>", state=DISABLED, pady=5)
+	else:
+		button_forward = Button(root, text=">>", command=lambda:forward(1), pady=5)
+
+	button_back.grid(row=1, column=0)
+	button_exit.grid(row=1, column=1)
+	button_forward.grid(row=1, column=2)
+
+def forward(num):
+	global myLabel
+	global button_forward
+	global button_back
+
+	if len(ImageList)<num+1:
+		load(num)
+		printImg(num)
+	else:
+		printImg(num)
+
+	button_forward = Button(root, text=">>", command=lambda:forward(num+1))
+	button_back = Button(root, text="<<", command=lambda:back(num-1))
+	myLabel.grid(row=0, column=0, columnspan=3)
+	if num < (len(root.filename)-1):
+		button_back.grid(row=1, column=0)
+		button_forward.grid(row=1, column=2)
+	else:
+		button_forward = Button(root, text=">>", command=lambda:forward(num+1), state=DISABLED)
+		button_back.grid(row=1, column=0)
+		button_forward.grid(row=1, column=2)
+
+def back(num):
+	global myLabel
+	global button_forward
+	global button_back
+	
+	printImg(num)
+
+	button_forward = Button(root, text=">>", command=lambda:forward(num+1))
+	if num == 0:
+		button_back = Button(root, text="<<", state=DISABLED)
+	else:
+		button_back = Button(root, text="<<", command=lambda:back(num-1))
+	myLabel.grid(row=0, column=0, columnspan=3)
+	button_back.grid(row=1, column=0)
+	button_forward.grid(row=1, column=2)
+
+def openfiles():
+	del root.filename
+	root.filename = filedialog.askopenfilenames(initialdir="/home/ruka/Anime&Manga", 
+											title="Select file", 
+											filetypes=(("webp file",".webp"),("all file",".*")))
+	if not root.filename:
+		quit()
+	global ImageList
+	ImageList=[]
+	load(0)
+	printImg(0)
+	inibutton()
+
+openfile()
+load(0)
+iniprintImg(0)
+inibutton()
+
+
+root.mainloop()
