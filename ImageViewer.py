124d763617ff8a885fd3da284d8bb3dcc5e912ff
g09050zz <zhongtian.zheng@student.manchester.ac.uk>
2020-12-04 01:19:13 +0000

    ImageViewer_v1.03

--- ImageViewer.py
+++ ImageViewer.py
@@ -1,12 +1,12 @@
 from tkinter import *
+from tkinter import filedialog, messagebox
 from PIL import ImageTk, Image
-from tkinter import filedialog
 
 root = Tk()
 root.title('Image Viewer')
-
 root.filename=Label(root)
 myLabel=Label(root)
+myLabel.grid(row=0, column=0, columnspan=3)
 
 def resize(img):
 	width = img.size[0]
@@ -25,68 +25,56 @@ def load(num):
 
 def printImg(num):
 	global myLabel
-	myLabel.grid_forget()
-	myLabel=Label(image=ImageList[num])
-	myLabel.grid(row=0, column=0, columnspan=3)
+	myLabel.config(image=ImageList[num])
 	status = Label(root,text="Image "+str(num+1)+" of "+str(len(root.filename))+"	", bd=1, relief=SUNKEN, anchor=E)
 	status.grid(row=2,column=0,columnspan=3, sticky=W+E)
 
 def inibutton():
+	global button_forward
+	global button_back
 	button_back = Button(root, text="<<", state=DISABLED)
 	button_exit = Button(root, text="OPEN", bg='#d97b7b', command=openfiles)
+	button_forward = Button(root, text=">>", command=lambda:forward(1))
 	if len(root.filename)==1:
-		button_forward = Button(root, text=">>", state=DISABLED)
-	else:
-		button_forward = Button(root, text=">>", command=lambda:forward(1))
+		button_forward.config(state=DISABLED)
 
 	button_back.grid(row=1, column=0)
 	button_exit.grid(row=1, column=1)
 	button_forward.grid(row=1, column=2)
 
 def forward(num):
-	global myLabel
-	global button_forward
-	global button_back
 	if len(ImageList)<num+1:
 		load(num)
-		printImg(num)
-	else:
-		printImg(num)
-	button_forward = Button(root, text=">>", command=lambda:forward(num+1))
-	button_back = Button(root, text="<<", command=lambda:back(num-1))
-	myLabel.grid(row=0, column=0, columnspan=3)
-	if num < (len(root.filename)-1):
-		button_back.grid(row=1, column=0)
-		button_forward.grid(row=1, column=2)
-	else:
-		button_forward = Button(root, text=">>", command=lambda:forward(num+1), state=DISABLED)
-		button_back.grid(row=1, column=0)
-		button_forward.grid(row=1, column=2)
+	printImg(num)
+	button_forward.config(command=lambda:forward(num+1))
+	button_back.config(command=lambda:back(num-1), state=NORMAL)
+	if num == (len(root.filename)-1):
+		button_forward.config(state=DISABLED)
 
 def back(num):
-	global myLabel
-	global button_forward
-	global button_back	
 	printImg(num)
-	button_forward = Button(root, text=">>", command=lambda:forward(num+1))
+	button_forward.config(command=lambda:forward(num+1), state=NORMAL)
+	button_back.config(command=lambda:back(num-1))
 	if num == 0:
-		button_back = Button(root, text="<<", state=DISABLED)
-	else:
-		button_back = Button(root, text="<<", command=lambda:back(num-1))
-	myLabel.grid(row=0, column=0, columnspan=3)
-	button_back.grid(row=1, column=0)
-	button_forward.grid(row=1, column=2)
-
+		button_back.config(state=DISABLED)
+		
 def openfiles():
+	if root.filename:
+		backup = root.filename
 	del root.filename
 	root.filename = filedialog.askopenfilenames(initialdir="/home/ruka/Anime&Manga", title="Select file", filetypes=(("all file",".*"), ("webp file",".webp"),("jpg file",".jpg"),("png file",".png")))
 	if not root.filename:
-		quit()
-	global ImageList
-	ImageList=[]
-	load(0)
-	printImg(0)
-	inibutton()
+		exitComfirm = messagebox.askyesno("Exit the Programme?","You didn't choose any file. Exit the programme?")
+		if exitComfirm == 1:
+			quit()
+		else:
+			root.filename = backup
+	else:
+		global ImageList
+		ImageList=[]
+		load(0)
+		printImg(0)
+		inibutton()
 
 openfiles()
 root.mainloop()
